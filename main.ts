e5336f33180147b4a925f8624548d4d773b137f9
Kabir-iqbal <161919126+Kabir-Iqbal@users.noreply.github.com>
2024-07-12 13:22:00 +0100

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,27 @@
+// 32. Checking Usernames: 
+// Do the following to create a program that simulates how websites ensure that everyone has a 
+// unique username.
+// • Make a list of five or more usernames called current_users.
+// • Make another list of five usernames called new_users. Make sure one or two of the new usernames 
+// are also in the current_users list.
+// • Loop through the new_users list to see if each new username has already been used. If it has, print 
+// a message that the person will need to enter a new username. If a username has not been used, print 
+// a message saying that the username is available.
+// • Make sure your comparison is case insensitive. If 'John' has been used, 'JOHN' should not be 
+// accepted.
+let current_users = ["Ali","Übaid","Kaser","Rahib","Razaq"]
+let new_users = ["Hashim","Hussain","Rehan","Ali","Kaser"]
+// we will use to for each method 
+new_users.forEach(new_user =>{
+    let newuserlower = new_user.toLowerCase();
+
+    // we wil use to some method to connect lists
+    let alreadytaken = current_users.some(current_user => current_user.toLowerCase() === newuserlower)
+    if (alreadytaken) {
+        console.log(`${new_user} is already taken try another username.`);
+        
+    }else{
+        console.log(`${new_user} is added to username.`);
+        
+    }
+})
